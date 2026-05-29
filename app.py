362d71c9ff0b75b3f8c9fd036bc5579d2897f3ce
malinitesh-sketch <malinitesh2922008@gmail.com>
2026-05-29 09:12:17 +0530

    Add Dockerfile for Render deploy

--- app.py
+++ app.py
@@ -231,4 +231,5 @@ def api_download():
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
